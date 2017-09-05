2ead9facf4b7bf67822d5deed3a1bd50fb322c5f
Bart van Kuik <bart@dutchvirtual.nl>
2017-09-05 12:01:33 +0200

    Added simple spinner, without static member and animatinos

---
+++ Spinner/VISimpleSpinnerView.swift
@@ -0,0 +1,99 @@
+//
+//  VISimpleSpinnerView.swift
+//  Spinner
+//
+//  Created by Bart van Kuik on 05/09/2017.
+//  Copyright © 2017 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+
+public class VISimpleSpinnerView: UIView {
+
+    internal let label = UILabel()
+    private let stackView = UIStackView()
+    private let activityIndicatorView: UIActivityIndicatorView
+    private var constraintsInstalled = false
+    private let minimumDimension: CGFloat = 150.0
+    private let margin: CGFloat = 20.0
+
+    // MARK: - Public functions
+
+    public func show(text: String, in containingView: UIView) {
+        self.label.text = text
+
+        self.centerView(in: containingView)
+        self.isHidden = false
+    }
+
+    public func hide() {
+        self.isHidden = true
+    }
+
+    // MARK: - Private functions
+
+    private func centerView(in containingView: UIView) {
+        if containingView != self.superview {
+            self.removeFromSuperview()
+            containingView.addSubview(self)
+            let constraints = [
+                self.centerXAnchor.constraint(equalTo: containingView.centerXAnchor),
+                self.centerYAnchor.constraint(equalTo: containingView.centerYAnchor),
+                self.widthAnchor.constraint(lessThanOrEqualTo: containingView.widthAnchor, multiplier: 1.0, constant: -10)
+            ]
+            containingView.addConstraints(constraints)
+        }
+
+        containingView.bringSubview(toFront: self)
+    }
+
+    // MARK: - Layout
+
+    override public func layoutSubviews() {
+        if !self.constraintsInstalled {
+            self.constraintsInstalled = true
+
+            let constraints = [
+                self.stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: self.margin),
+                self.stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -self.margin),
+                self.stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: self.margin),
+                self.stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -self.margin),
+                self.widthAnchor.constraint(greaterThanOrEqualToConstant: self.minimumDimension),
+                self.heightAnchor.constraint(greaterThanOrEqualToConstant: self.minimumDimension),
+                ]
+            self.addConstraints(constraints)
+        }
+
+        super.layoutSubviews()
+    }
+
+    // MARK: - Life cycle
+
+    override init(frame: CGRect) {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.white
+        label.lineBreakMode = .byTruncatingTail
+        activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        activityIndicatorView.startAnimating()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = self.margin
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        stackView.addArrangedSubview(activityIndicatorView)
+        stackView.addArrangedSubview(label)
+
+        super.init(frame: frame)
+        self.addSubview(stackView)
+        self.isHidden = true
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = UIColor.gray
+        self.layer.cornerRadius = 5.0
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("Unsupported")
+    }
+}
+
--- VISpinnerViewExample/ViewController.swift
+++ VISpinnerViewExample/ViewController.swift
@@ -14,21 +14,33 @@ class ViewController: UIViewController {
 
     private let spinnerShowButton = UIButton()
     private let spinnerHideButton = UIButton()
+    private let simpleSpinnerShowButton = UIButton()
+    private let simpleSpinnerHideButton = UIButton()
     private let toastViewButton = UIButton()
     private let topView = UIView()
     private let centerView = UIView()
     private let bottomView = UIView()
     private var spinnerText = "a"
+    private let simpleSpinnerView = VISimpleSpinnerView()
 
     func spinnerShowButtonTouchUpInside(sender: UIButton) {
         self.spinnerText += "a"
         VISpinnerView.show(text: self.spinnerText, in: self.topView)
     }
-    
+
     func spinnerHideButtonTouchUpInside(sender: UIButton) {
         VISpinnerView.hide()
     }
-    
+
+    func simpleSpinnerShowButtonTouchUpInside(sender: UIButton) {
+        self.spinnerText += "a"
+        self.simpleSpinnerView.show(text: self.spinnerText, in: self.centerView)
+    }
+
+    func simpleSpinnerHideButtonTouchUpInside(sender: UIButton) {
+        self.simpleSpinnerView.hide()
+    }
+
     func toastViewButtonTouchUpInside(sender: UIButton) {
         let dateFormat = DateFormatter()
         dateFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
@@ -52,19 +64,35 @@ class ViewController: UIViewController {
         self.spinnerHideButton.setTitleColor(UIColor.blue, for: .normal)
         self.spinnerHideButton.addTarget(self, action: #selector(spinnerHideButtonTouchUpInside(sender:)), for: .touchUpInside)
         self.view.addSubview(self.spinnerHideButton)
+        
+        self.simpleSpinnerShowButton.translatesAutoresizingMaskIntoConstraints = false
+        self.simpleSpinnerShowButton.setTitle("Show simple spinner", for: .normal)
+        self.simpleSpinnerShowButton.setTitleColor(UIColor.blue, for: .normal)
+        self.simpleSpinnerShowButton.addTarget(self, action: #selector(simpleSpinnerShowButtonTouchUpInside(sender:)), for: .touchUpInside)
+        self.view.addSubview(self.simpleSpinnerShowButton)
 
+        self.simpleSpinnerHideButton.translatesAutoresizingMaskIntoConstraints = false
+        self.simpleSpinnerHideButton.setTitle("Hide simple spinner", for: .normal)
+        self.simpleSpinnerHideButton.setTitleColor(UIColor.blue, for: .normal)
+        self.simpleSpinnerHideButton.addTarget(self, action: #selector(simpleSpinnerHideButtonTouchUpInside(sender:)), for: .touchUpInside)
+        self.view.addSubview(self.simpleSpinnerHideButton)
+        
         self.toastViewButton.translatesAutoresizingMaskIntoConstraints = false
         self.toastViewButton.setTitle("Show toast", for: .normal)
         self.toastViewButton.setTitleColor(UIColor.blue, for: .normal)
         self.toastViewButton.addTarget(self, action: #selector(toastViewButtonTouchUpInside(sender:)), for: .touchUpInside)
         self.view.addSubview(self.toastViewButton)
 
+        self.toastViewButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.toastViewButton.bottomAnchor.constraint(equalTo: self.simpleSpinnerShowButton.topAnchor).isActive = true
+        self.simpleSpinnerShowButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.simpleSpinnerShowButton.bottomAnchor.constraint(equalTo: self.simpleSpinnerHideButton.topAnchor).isActive = true
+        self.simpleSpinnerHideButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.simpleSpinnerHideButton.bottomAnchor.constraint(equalTo: self.spinnerShowButton.topAnchor).isActive = true
         self.spinnerShowButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         self.spinnerShowButton.bottomAnchor.constraint(equalTo: self.spinnerHideButton.topAnchor).isActive = true
         self.spinnerHideButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         self.spinnerHideButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        self.toastViewButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        self.toastViewButton.bottomAnchor.constraint(equalTo: self.spinnerShowButton.topAnchor).isActive = true
 
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
